a02c2da0fd4919fa27a94a7579b145e62de010bb
Jinyoung Park <146058962+jinjero@users.noreply.github.com>
2024-08-19 13:51:55 +0900

    feat: Implement register, login pages (#9)
    * feat: Implement register, login pages
    
    #3
    
    * feat: Link a pages
    
    #3
    
    * feat: Add styles
    
    #3

--- IM/src/routes/router.tsx
+++ IM/src/routes/router.tsx
@@ -2,14 +2,24 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import Chat from "../pages/Chat/Chat";
 import ChatList from "../pages/ChatList/ChatList";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
 
 export const router = createBrowserRouter([
-    {
-        path : '/',
-        element: <Home/>
-    },
-    {
-        path: '/chat',
-        element : <ChatList />
-    }
-])
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/chat",
+    element: <ChatList />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
