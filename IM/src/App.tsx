5a24acfab05eb2b3ce3dfab0bbbd481026b3ebac
joamksh <joamksh@naver.com>
2024-08-31 21:04:56 +0900

    feat : json형식으로 차트구현

--- IM/src/App.tsx
+++ IM/src/App.tsx
@@ -1,18 +1,22 @@
+// import React from "react";
 import { RouterProvider } from "react-router-dom";
 import Layout from "./routes/Layout";
 import { router } from "./routes/router";
 import Header from "./components/common/Header/Header";
-import Chart from "./pages/Chart/Chart.tsx";
+import CandlestickChart from './CandlestickChart';
+
+
 function App() {
-  return (
-    <>
-      <Layout>
-        <Header />
-          <Chart />
-        <RouterProvider router = {router}/>
-      </Layout>
-    </>
-  );
+    return (
+        <>
+            <Layout>
+
+                <Header />
+                <RouterProvider router={router} />
+                <CandlestickChart />
+            </Layout>
+        </>
+    );
 }
 
 export default App;
