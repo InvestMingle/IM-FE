a3cd828b2b7bb1402a1cf3c7b569148ec0c54e7e
joamksh <joamksh@naver.com>
2024-09-02 15:18:33 +0900

    ref : 커밋 정리

--- IM/src/App.tsx
+++ IM/src/App.tsx
@@ -3,19 +3,18 @@ import { RouterProvider } from "react-router-dom";
 import Layout from "./routes/Layout";
 import { router } from "./routes/router";
 import Header from "./components/common/Header/Header";
-import CandlestickChart from './CandlestickChart';
-
+import Chart from "./pages/Chart/Chart.tsx"
 
 function App() {
-
-  return (
-    <>
-      <Layout>
-        <Header />
-        <RouterProvider router={router} />
-      </Layout>
-    </>
-  );
+    return (
+        <>
+            <Layout>
+                <Header />
+                <RouterProvider router={router} />
+                <Chart />
+            </Layout>
+        </>
+    );
 }
 
 export default App;
